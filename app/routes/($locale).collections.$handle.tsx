4fe43700fc4550adecd37f9977000706466503d8
Md. Omar Faruq <shuvo.omar09@gmail.com>
2024-03-14 11:15:51 +0600

    new feature added

--- app/routes/($locale).collections.$handle.tsx
+++ app/routes/($locale).collections.$handle.tsx
@@ -1,21 +1,24 @@
-import {json, redirect, type LoaderFunctionArgs} from '@shopify/remix-oxygen';
-import {useLoaderData, Link, type MetaFunction} from '@remix-run/react';
+import { json, redirect, type LoaderFunctionArgs } from '@shopify/remix-oxygen';
+import { useLoaderData, Link, type MetaFunction } from '@remix-run/react';
 import {
   Pagination,
   getPaginationVariables,
   Image,
   Money,
 } from '@shopify/hydrogen';
-import type {ProductItemFragment} from 'storefrontapi.generated';
-import {useVariantUrl} from '~/lib/variants';
+import type { ProductItemFragment } from 'storefrontapi.generated';
+import { useVariantUrl } from '~/lib/variants';
+import infoIcon from "../../public/icon_info.svg";
+import leftArrow from "../../public/left_arrow.svg";
+import rightArrow from "../../public/right_arrow.svg";
 
-export const meta: MetaFunction<typeof loader> = ({data}) => {
-  return [{title: `Hydrogen | ${data?.collection.title ?? ''} Collection`}];
+export const meta: MetaFunction<typeof loader> = ({ data }) => {
+  return [{ title: `Hydrogen | ${data?.collection.title ?? ''} Collection` }];
 };
 
-export async function loader({request, params, context}: LoaderFunctionArgs) {
-  const {handle} = params;
-  const {storefront} = context;
+export async function loader({ request, params, context }: LoaderFunctionArgs) {
+  const { handle } = params;
+  const { storefront } = context;
   const paginationVariables = getPaginationVariables(request, {
     pageBy: 8,
   });
@@ -24,8 +27,8 @@ export async function loader({request, params, context}: LoaderFunctionArgs) {
     return redirect('/collections');
   }
 
-  const {collection} = await storefront.query(COLLECTION_QUERY, {
-    variables: {handle, ...paginationVariables},
+  const { collection } = await storefront.query(COLLECTION_QUERY, {
+    variables: { handle, ...paginationVariables },
   });
 
   if (!collection) {
@@ -33,11 +36,11 @@ export async function loader({request, params, context}: LoaderFunctionArgs) {
       status: 404,
     });
   }
-  return json({collection});
+  return json({ collection });
 }
 
 export default function Collection() {
-  const {collection} = useLoaderData<typeof loader>();
+  const { collection } = useLoaderData<typeof loader>();
   fetchMenuItems().then((menuItems) => {
     if (menuItems) {
       console.log('Menu Items:', JSON.stringify(menuItems));
@@ -49,11 +52,18 @@ export default function Collection() {
 
   return (
     <div className="collection">
-      <h1>{collection.title}</h1>
-      <p className="collection-description">{collection.description}</p>
+      <div className='flex justify-between items-center'>
+        <h1 className='font-bold text-[32px] text-[#323232]'>{collection.title}</h1>
+        <div className='flex justify-around'>
+          <img src={leftArrow} alt='Left' className='mr-2 w-8 h-8' />
+          <img src={rightArrow} alt='Right' className='w-8 h-8' />
+        </div>
+      </div>
+      {/* <p className="collection-description">{collection.description}</p> */}
       <Pagination connection={collection.products}>
-        {({nodes, isLoading, PreviousLink, NextLink}) => (
+        {({ nodes, isLoading, PreviousLink, NextLink }) => (
           <>
+
             <PreviousLink>
               {isLoading ? 'Loading...' : <span>↑ Load previous</span>}
             </PreviousLink>
@@ -87,9 +97,9 @@ async function fetchMenuItems() {
   }
 }
 
-function ProductsGrid({products}: {products: ProductItemFragment[]}) {
+function ProductsGrid({ products }: { products: ProductItemFragment[] }) {
   return (
-    <div className="products-grid">
+    <div className="/* products-grid */ flex justify-around">
       {products.map((product, index) => {
         return (
           <ProductItem
@@ -113,26 +123,36 @@ function ProductItem({
   const variant = product.variants.nodes[0];
   const variantUrl = useVariantUrl(product.handle, variant.selectedOptions);
   return (
-    <Link
-      className="product-item"
-      key={product.id}
-      prefetch="intent"
-      to={variantUrl}
-    >
-      {product.featuredImage && (
-        <Image
-          alt={product.featuredImage.altText || product.title}
-          aspectRatio="1/1"
-          data={product.featuredImage}
-          loading={loading}
-          sizes="(min-width: 45em) 400px, 100vw"
-        />
-      )}
-      <h4>{product.title}</h4>
-      <small>
-        <Money data={product.priceRange.minVariantPrice} />
-      </small>
-    </Link>
+    <div className='flex justify-center w-[246px] h-[310px]'>
+      <Link
+        className="flex flex-col items-center product-item"
+        key={product.id}
+        prefetch="intent"
+        to={variantUrl}
+      >
+        <div className='mt-0.5 w-[186px] h-[186px] relative'>
+          <img className='[&&]:w-[20px] [&&]:h-[20px] mt-[-2px] ml-[170px] absolute' src={infoIcon} alt="image_import" />
+
+          {product.featuredImage && (
+            <Image
+              alt={product.featuredImage.altText || product.title}
+              aspectRatio="1/1"
+              data={product.featuredImage}
+              loading={loading}
+              sizes="(min-width: 45em) 400px, 100vw"
+              className='p-2'
+            />
+          )}
+        </div>
+        <div className='bg-[#D3B5D1] rounded-[14px] w-[246px] h-[212px] mt-[-98px] -z-10 flex flex-col items-center' style={{ /* backgroundColor: "rgba(211, 181, 209, 0.22)", */ }}>
+          <h4 className='mt-[90px] font-bold text-[18px]'>{product.title}</h4>
+          <small className='font-semibold text-[14px] text-[#9C6EAA]'>
+            <Money data={product.priceRange.minVariantPrice} />
+          </small>
+          <button className='w-[206px] h-[50px] rounded-full border-0 bg-[#FFAD05] mt-[10px] mb-[20px] font-bold text-[20px] text-white tracking-wide'>PUT I KURV</button>
+        </div>
+      </Link >
+    </div >
   );
 }
 
