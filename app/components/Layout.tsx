fe38289d8861448bd9400beab13c0bc226917804
Md. Omar Faruq <shuvo.omar09@gmail.com>
2024-03-28 20:24:05 +0600

    notification toaster step1

--- app/components/Layout.tsx
+++ app/components/Layout.tsx
@@ -13,6 +13,7 @@ import {
   PredictiveSearchForm,
   PredictiveSearchResults,
 } from '~/components/Search';
+import right_arrow from '../../public/right_arrow_sign.svg';
 
 export type LayoutProps = {
   cart: Promise<CartApiQueryFragment | null>;
@@ -41,6 +42,28 @@ export function Layout({
           {(footer) => <Footer menu={footer?.menu} shop={header?.shop} />}
         </Await> */}
       </Suspense>
+      <div className="flex flex-col items-center w-[100%] h-[88px] bg-[#6E4695] sticky  bottom-0 z-[1]">
+        <div className="w-[308px]">
+          <div className="h-8 flex flex-nowrap items-center">
+            <div className="w-full bg-[#D3B5D1] rounded-full h-1.5">
+              <div className="bg-white h-1.5 w-[20%] rounded-full"></div>
+            </div>
+            <p className="text-nowrap text-white ml-1.5">Step 1 of 5</p>
+          </div>
+          <div className="h-12 rounded-xl bg-[#C7F0BD] px-[20px] py-3 flex justify-center">
+            <button className="font-bold text-base text-[#323232] flex justify-between items-center">
+              <p className="ml-9">NEXT - SELECT CANDY</p>
+              <img
+                className="ml-12"
+                src={right_arrow}
+                alt=""
+                width={6}
+                height={10}
+              />
+            </button>
+          </div>
+        </div>
+      </div>
     </>
   );
 }
