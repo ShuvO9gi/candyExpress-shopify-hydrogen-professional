1e48bbb13346507556c71123576e6b19f342aeb8
Md. Omar Faruq <shuvo.omar09@gmail.com>
2024-04-03 21:29:23 +0600

    search icon added

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -6,6 +6,7 @@ import {useRootLoaderData} from '~/root';
 import page_logo from '../../public/page_logo.svg';
 import cart_black_logo from '../../public/cart_black_logo.svg';
 import hamburger_icon from '../../public/hamburger_icon.svg';
+import searchIcon from '../../public/search_black_icon.svg';
 
 type HeaderProps = Pick<LayoutProps, 'header' | 'cart' | 'isLoggedIn'>;
 
@@ -95,10 +96,7 @@ function HeaderCtas({
   cart,
 }: Pick<HeaderProps, 'isLoggedIn' | 'cart'>) {
   return (
-    <nav
-      className="flex justify-between w-[83%] h-[64%] ml-8"
-      role="navigation"
-    >
+    <nav className="flex items-center w-full h-full ml-7" role="navigation">
       <HeaderMenuMobileToggle />
       {/* <NavLink prefetch="intent" to="/account" style={activeLinkStyle}>
         <Suspense fallback="Sign in">
@@ -107,7 +105,7 @@ function HeaderCtas({
           </Await>
         </Suspense>
       </NavLink> */}
-      {/* <SearchToggle /> */}
+      <SearchToggle />
       <CartToggle cart={cart} />
     </nav>
   );
@@ -115,24 +113,31 @@ function HeaderCtas({
 
 function HeaderMenuMobileToggle() {
   return (
-    <a
-      className="header-menu-mobile-toggle flex justify-center"
-      href="#mobile-menu-aside"
-    >
-      <img src={hamburger_icon} alt="Toggle menu" width={22} height={16} />
+    <a className="mt-5" href="#mobile-menu-aside">
+      <img
+        className="rounded-none"
+        src={hamburger_icon}
+        alt="Toggle menu"
+        width={20}
+        height={16}
+      />
     </a>
   );
 }
 
-/* function SearchToggle() {
-  return <a href="#search-aside">Search</a>;
-} */
+function SearchToggle() {
+  return (
+    <a className="ml-[250px] mt-5" href="#search-aside">
+      <img src={searchIcon} alt="Search" width={20} height={20} />
+    </a>
+  );
+}
 
 function CartBadge({count}: {count: number}) {
   return (
-    <div className="w-9 h-8 relative">
+    <div className="w-9 h-8 ml-4 relative">
       <a className="absolute top-4" href="/cart">
-        <img src={cart_black_logo} alt="Cart" width={17} height={17} />
+        <img src={cart_black_logo} alt="Cart" width={18} height={18} />
       </a>
       <div className="w-[22px] h-[22px] bg-[#FFAD05] rounded-full absolute top-[2px] left-[14px] text-white font-bold text-lg flex justify-center items-center">
         {count}
