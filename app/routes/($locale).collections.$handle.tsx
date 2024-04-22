1badb947a80370091ac5dc7bc918497fc8e75fe3
Md. Omar Faruq <shuvo.omar09@gmail.com>
2024-04-23 03:30:23 +0600

    search functionality added

--- app/routes/($locale).collections.$handle.tsx
+++ app/routes/($locale).collections.$handle.tsx
@@ -11,6 +11,9 @@ import {useVariantUrl} from '~/lib/variants';
 import infoIcon from '../../public/icon_info.svg';
 import leftArrow from '../../public/left_arrow.svg';
 import rightArrow from '../../public/right_arrow.svg';
+import closeBlackIcon from '../../public/action/close_icon.svg';
+import searchBlackIcon from '../../public/action/search_black_icon.svg';
+import searchWhiteIcon from '../../public/action/search_white_icon.svg';
 import React, {useEffect, useRef, useState} from 'react';
 
 import type {
@@ -115,47 +118,179 @@ function ProductsGrid({
   products: ProductItemFragment[];
   categories: Category[];
 }) {
+  /*  */
+  const [searchCandy, setSearchCandy] = useState(false);
+  const [searchQuery, setSearchQuery] = useState('');
+  const [filteredItems, setFilteredItems] = useState<ProductItemFragment[]>([]);
+
+  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const query = event.target.value;
+    setSearchQuery(query);
+
+    const filtered = products.filter((item: ProductItemFragment) =>
+      item.title.toLowerCase().includes(query.toLowerCase()),
+    );
+
+    setFilteredItems(filtered);
+  };
+
   return (
-    <div>
-      {categories.map((category, index) => {
-        const filteredProducts = products.filter((product) =>
-          product.tags.includes(category.tag_name),
-        );
+    <>
+      <div>
+        {searchQuery === '' ? (
+          <div>
+            {categories.map((category, index) => {
+              const filteredProducts = products.filter((product) =>
+                product.tags.includes(category.tag_name),
+              );
 
-        if (!filteredProducts.length) return null;
+              if (!filteredProducts.length) return null;
 
-        return (
-          <div key={category.tag_name}>
-            <div className="flex justify-between items-center md:mb-8 mb-4">
-              <h1 className="md:text-4xl text-2xl font-bold">
-                {category.display_name}
-              </h1>
-            </div>
+              return (
+                <div key={category.tag_name}>
+                  <div className="flex justify-between items-center md:mb-8 mb-4">
+                    <h1 className="md:text-4xl text-2xl font-bold">
+                      {category.display_name}
+                    </h1>
+                  </div>
 
-            <Swiper
-              slidesPerView={'auto'}
-              spaceBetween={10}
-              navigation
-              scrollbar={{hide: true}}
-              speed={500}
-              threshold={10}
-              style={{
-                display: 'flex',
-                overflowX: 'hidden',
-                whiteSpace: 'nowrap',
-                flexDirection: 'row',
-              }}
-            >
-              {filteredProducts.map((product, idx) => (
-                <SwiperSlide key={`${product.id}-${idx}`} className="md:mr-5">
-                  <ProductItem product={product} />
-                </SwiperSlide>
-              ))}
-            </Swiper>
+                  <Swiper
+                    slidesPerView={'auto'}
+                    spaceBetween={10}
+                    navigation
+                    scrollbar={{hide: true}}
+                    speed={500}
+                    threshold={10}
+                    style={{
+                      display: 'flex',
+                      overflowX: 'hidden',
+                      whiteSpace: 'nowrap',
+                      flexDirection: 'row',
+                    }}
+                  >
+                    {filteredProducts.map((product, idx) => (
+                      <SwiperSlide
+                        key={`${product.id}-${idx}`}
+                        className="md:mr-5"
+                      >
+                        <ProductItem product={product} />
+                      </SwiperSlide>
+                    ))}
+                  </Swiper>
+                </div>
+              );
+            })}
           </div>
-        );
-      })}
-    </div>
+        ) : filteredItems.length > 0 ? (
+          <div>
+            {categories.map((category, index) => {
+              const filteredProducts = filteredItems.filter((product) =>
+                product.tags.includes(category.tag_name),
+              );
+
+              if (!filteredProducts.length) return null;
+
+              return (
+                <div key={category.tag_name}>
+                  <div className="flex justify-between items-center md:mb-8 mb-4">
+                    <h1 className="md:text-4xl text-2xl font-bold">
+                      {category.display_name}
+                    </h1>
+                  </div>
+
+                  <Swiper
+                    slidesPerView={'auto'}
+                    spaceBetween={10}
+                    navigation
+                    scrollbar={{hide: true}}
+                    speed={500}
+                    threshold={10}
+                    style={{
+                      display: 'flex',
+                      overflowX: 'hidden',
+                      whiteSpace: 'nowrap',
+                      flexDirection: 'row',
+                    }}
+                  >
+                    {filteredProducts.map((product, idx) => (
+                      <SwiperSlide
+                        key={`${product.id}-${idx}`}
+                        className="md:mr-5"
+                      >
+                        <ProductItem product={product} />
+                      </SwiperSlide>
+                    ))}
+                  </Swiper>
+                </div>
+              );
+            })}
+          </div>
+        ) : (
+          <div></div>
+        )}
+      </div>
+      {searchCandy && (
+        <div className="w-full h-[80px] bg-[#333333]/50 sticky bottom-[28px] lg:bottom-[124px] z-[1] flex justify-center items-center">
+          <input
+            type="search"
+            className="w-[50%] h-[50%] rounded text-sm font-normal text-black/50"
+            placeholder="Search your candy"
+            onChange={(e) => handleInputChange(e)}
+          />
+          <button
+            className="w-7 h-7 bg-[#FFAD05] flex justify-center items-center absolute right-0 top-0"
+            onClick={() => {
+              setSearchCandy(false);
+              setSearchQuery('');
+            }}
+          >
+            <img
+              className=""
+              src={closeBlackIcon}
+              alt="close"
+              width={12}
+              height={12}
+            />
+          </button>
+        </div>
+      )}
+      {/* {!searchCandy && ( */}
+      <div className="lg:hidden w-[200px] h-7 bg-[#C7F0BD] rounded-se-lg sticky bottom-[0px] z-[1] flex items-center">
+        <div className="ml-7 flex items-center text-xs font-semibold">
+          Search Product Here
+          <span
+            className="ml-3"
+            onClick={() => setSearchCandy(true)}
+            onKeyDown={() => setSearchCandy(true)}
+            role="button"
+            tabIndex={0}
+          >
+            <img src={searchBlackIcon} alt="Search" width={16} height={16} />
+          </span>
+        </div>
+      </div>
+      {/* )} */}
+      <div className="w-full h-[124px] bg-[#6E4695] sticky bottom-[0px] hidden lg:flex justify-evenly items-center">
+        <div className="w-48 h-16 text-white">
+          <p className="text-xl">Vægt: 350 g</p>
+          <div className="font-bold text-[32px]">58,50 DKK</div>
+        </div>
+        <div className="w-[670px] flex justify-between items-center">
+          <button
+            className="hidden w-[50px] h-[50px] rounded-lg bg-[#FFAD05] lg:flex justify-center items-center"
+            onClick={() => setSearchCandy(true)}
+          >
+            <img src={searchWhiteIcon} alt="Search" width={24} height={24} />
+          </button>
+          <div className="w-72 h-[60px] rounded-full border-2 border-white flex items-center justify-center uppercase text-white font-bold text-xl">
+            Watch your bag
+          </div>
+          <div className="w-72 h-[60px] rounded-full bg-[#FFAD05] flex items-center justify-center uppercase text-white font-bold text-xl">
+            Buy for 10 nok more
+          </div>
+        </div>
+      </div>
+    </>
   );
 }
 
